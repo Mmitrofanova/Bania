e27fa5192b36bdfabf4561e3675a6f49089272ba
Mmitrofanova <m.mitrofanova@meta.ua>
2016-11-28 22:03:37 +0200

    Add files via upload

---
+++ javaS.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+  $('.sl').slick({
+  autoplay: true,
+  autoplaySpeed: 2000,
+  speed: 3000,
+  dots: true,
+  // fade: true,
+  });
+/*  $('.sl').slick({
+  cssEase: 'ease-in',
+  asNavFor: '.sl2',
+  initialSlide: 11,
+  // infinite: true,
+  autoplay: true,
+  autoplaySpeed: 2000
+});
+
+$('.sl2').slick({
+  dots: true,
+  slidesToShow: 7,
+  initialSlide: 11,
+  centerMode: true,
+  centerPadding: '40px',
+  asNavFor: '.sl',
+  focusOnSelect: true,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1100,
+      settings: {
+        slidesToShow: 6
+      }
+    },
+    {
+      breakpoint: 960,
+      settings: {
+        slidesToShow: 5
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 4
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        dots: false,
+        slidesToShow: 3,
+        centerMode: false
+      }
+    }
+  ]
+});*/
+
+});
